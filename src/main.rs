1fa569f8e77afe18c65fb4c47105a7fc7573e146
Jasper Visser <jasper.visser@keylane.com>
2020-05-18 11:38:27 +0200

    c

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ use proc_macro_hack::proc_macro_hack;
 extern crate proc_one;
 extern crate proc_two;
 
-/// Add one to an expression.
-///
-/// (Documentation goes here on the re-export, not in the other crate.)
 #[proc_macro_hack]
 use proc_one::one;
 #[proc_macro_hack]
