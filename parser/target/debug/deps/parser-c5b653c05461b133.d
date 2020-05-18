f2f4b4a070661c9ff3265497e24b2d65170e5f57
Jasper Visser <jasper.visser@keylane.com>
2020-05-18 10:49:48 +0200

    c

--- parser/target/debug/deps/parser-c5b653c05461b133.d
+++
@@ -1,5 +0,0 @@
-C:\Users\javisser\CLionProjects\proc_macro_collision\parser\target\debug\deps\parser-c5b653c05461b133.exe: src\lib.rs
-
-C:\Users\javisser\CLionProjects\proc_macro_collision\parser\target\debug\deps\parser-c5b653c05461b133.d: src\lib.rs
-
-src\lib.rs:
--- parser/target/debug/deps/unicode_xid-5ff2764f8250f5bd.d
+++
@@ -1,8 +0,0 @@
-C:\Users\javisser\CLionProjects\proc_macro_collision\parser\target\debug\deps\unicode_xid-5ff2764f8250f5bd.rmeta: C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\lib.rs C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\tables.rs
-
-C:\Users\javisser\CLionProjects\proc_macro_collision\parser\target\debug\deps\libunicode_xid-5ff2764f8250f5bd.rlib: C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\lib.rs C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\tables.rs
-
-C:\Users\javisser\CLionProjects\proc_macro_collision\parser\target\debug\deps\unicode_xid-5ff2764f8250f5bd.d: C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\lib.rs C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\tables.rs
-
-C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\lib.rs:
-C:\Users\javisser\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.0\src\tables.rs:
